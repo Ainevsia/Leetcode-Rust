5f137bf3bb2b16efb646493f286cf08775ad7cd2
Ainevsia <xuzhp0230@yeah.net>
2020-01-08 15:48:17 +0800

    template

--- 6. ZigZag Conversion/src/main.rs
+++ 6. ZigZag Conversion/src/main.rs
@@ -1,3 +1,22 @@
 fn main() {
     println!("Hello, world!");
 }
+
+pub struct Solution {}
+
+impl Solution {
+    pub fn convert(s: String, num_rows: i32) -> String {
+        println!("Hello, world!");
+        s
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Solution::convert(String::from("12"), 1), "12");
+    }
+}
