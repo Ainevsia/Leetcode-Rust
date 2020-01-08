1307086243f5b145c1e4d1596bf83e99c60e0871
Ainevsia <xuzhp0230@yeah.net>
2020-01-08 16:52:09 +0800

    pass

--- 6. ZigZag Conversion/src/main.rs
+++ 6. ZigZag Conversion/src/main.rs
@@ -1,13 +1,61 @@
 fn main() {
-    println!("Hello, world!");
+    let mut s = String::from("123");
+    s.push_str("123");
+    println!("{:?}", s);
+
+    let mut vec = Vec::new();
+    vec.push(String::from("123"));
+    vec[0].push_str("2");
+    vec.push(String::from("123"));
+    println!("{:?}", vec);
+
+    Solution::convert(String::from("0123456789"), 3);
+
 }
 
 pub struct Solution {}
 
 impl Solution {
     pub fn convert(s: String, num_rows: i32) -> String {
-        println!("Hello, world!");
-        s
+        if s.len() <= num_rows as usize || num_rows == 1 {
+            return s;
+        }
+        let s_iter = s.chars();
+        let mut cnt = 0;
+        let mut vec = Vec::new();
+        let mut up = false;
+        let group = num_rows - 1;
+        for c in s_iter {
+            // print!("{}", c);
+            if cnt < num_rows {
+                vec.push(c.to_string());
+            } else {
+                if cnt % group == 0 {
+                    if !up {
+                        vec[0].push(c);
+                        up = true;
+                    } else if up {
+                        vec[group as usize].push(c);
+                        up = false;
+                    }
+                }
+                else if !up {
+                    vec[(group - cnt % group) as usize].push(c);
+                } else if up {
+                    vec[(cnt % group) as usize].push(c);
+                }
+                // println!("{:?}", up);
+                // println!("{:?}", vec);
+            }
+            cnt += 1;
+        }
+        // println!("{:?}", vec);
+        let mut ret = String::new();
+        for i in 0..num_rows {
+            ret.push_str(&vec[i as usize]);
+        }
+        // println!("{}", ret);
+        ret
     }
 }
 
@@ -17,6 +65,9 @@ mod test {
 
     #[test]
     fn basic() {
-        assert_eq!(Solution::convert(String::from("12"), 1), "12");
+        assert_eq!(Solution::convert(String::from("PAYPALISHIRING"), 3), "PAHNAPLSIIGYIR");
+        assert_eq!(Solution::convert(String::from("PAYPALISHIRING"), 4), "PINALSIGYAHRPI");
+        assert_eq!(Solution::convert(String::from("123"), 4), "123");
+        assert_eq!(Solution::convert(String::from("123"), 1), "123");
     }
 }
