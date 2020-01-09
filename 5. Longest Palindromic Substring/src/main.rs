a3fa421ed66b1ff467ea41a2e81846be18988ef6
Ainevsia <xuzhp0230@yeah.net>
2020-01-09 15:15:06 +0800

    brute force

--- 5. Longest Palindromic Substring/src/main.rs
+++ 5. Longest Palindromic Substring/src/main.rs
@@ -1,13 +1,51 @@
 fn main() {
     println!("Hello, world!");
+
+    let mut bytes = "bors".bytes();
+
+    assert_eq!(Some(b'b'), bytes.next());
+    assert_eq!(Some(b'o'), bytes.next());
+    assert_eq!(Some(b'r'), bytes.next());
+    assert_eq!(Some(b's'), bytes.next());
+
+    assert_eq!(None, bytes.next());
+
 }
 
 pub struct Solution {}
 
 impl Solution {
     pub fn longest_palindrome(s: String) -> String {
-        
-        String::from("bab")
+        let vec : Vec<u8> = s.bytes().collect();
+        let len = vec.len();
+        if len <= 1 {
+            return s;
+        }
+        let mut start = 0;
+        let mut end = 0;
+        for s in 0..len {
+            for e in s+1..len {
+                if Solution::is_palindromic(&vec, s, e) && e - s > end - start {
+                    end = e;
+                    start = s;
+                }
+            }
+        }
+        s[start..end+1].into()
+    }
+
+    fn is_palindromic(s:&[u8], start:usize, end:usize) -> bool {
+        let mut end = end;
+        let mut m_start = start;
+        while m_start < end {
+            if s[m_start] != s[end] {
+                return false;
+            } else {
+                m_start += 1;
+                end -= 1;
+            }
+        }
+        true
     }
 }
 
