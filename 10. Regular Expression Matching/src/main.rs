fc8dc07070d9e91cd90b9d4c1a90fa1aaa023c31
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-14 19:48:54 +0800

    copy & paste & translate to rust ...XD

--- 10. Regular Expression Matching/src/main.rs
+++ 10. Regular Expression Matching/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Hello, world!");
-    Solution::is_match(String::from(""), String::from("aa"));
+    Solution::is_match(String::from(""), String::from(".*"));
 }
 
 struct Solution {}
@@ -8,24 +8,38 @@ struct Solution {}
 impl Solution {
     pub fn is_match(s: String, p: String) -> bool {
         let (ls, lp) = (s.len(), p.len());
-        if ls == 0 || lp == 0 {
-            return false;
-        }
-        let b = p.chars();
-        let mut dp = vec![vec![false; lp]; ls];
+        let mut dp = vec![vec![false; lp + 1]; ls + 1];
         dp[0][0] = true;
-        for j in 1..lp {
-            b.take(j);
-        }
-        let mut chars = "gravy train".chars().fuse();
-
-        while let Some(c) = chars.next() {
-            if c == 'x' {
-                chars.next(); // Skip the next one
+        for j in 0..lp {
+            if p.bytes().nth(j).unwrap() == b'*' && dp[0][j-1] {
+                dp[0][j+1] = true;
             }
         }
-        true
+        // println!("dp = {:?}", dp);
         
+        for i in 0..ls {
+            for j in 0..lp {
+                match p.bytes().nth(j).unwrap() {
+                    b'.'  => {
+                        dp[i+1][j+1] = dp[i][j];
+                    }
+                    b'*' => {
+                        if p.bytes().nth(j-1).unwrap() != s.bytes().nth(i).unwrap() &&
+                        p.bytes().nth(j-1).unwrap() != b'.' {
+                            dp[i+1][j+1] = dp[i+1][j-1];
+                        } else {
+                            dp[i+1][j+1] = dp[i+1][j] || dp[i][j+1] || dp[i+1][j-1];
+                        }
+                    }
+                    _ => {
+                        if s.bytes().nth(i).unwrap() == p.bytes().nth(j).unwrap() {
+                            dp[i+1][j+1] = dp[i][j];
+                        }
+                    }
+                }
+            }
+        }
+        dp[ls][lp]
     }
 }
 
@@ -41,5 +55,10 @@ mod test {
         assert_eq!(Solution::is_match(String::from("ab"), String::from("c*a*b")), true);
         assert_eq!(Solution::is_match(String::from("mississippi"), String::from("mis*is*p*.")), false);
         assert_eq!(Solution::is_match(String::from("mississippi"), String::from("mis*is*.p*.")), true);
+    }
+
+    #[test]
+    fn null() {
+        assert_eq!(Solution::is_match(String::from(""), String::from(".*")), true);
     }
 }
