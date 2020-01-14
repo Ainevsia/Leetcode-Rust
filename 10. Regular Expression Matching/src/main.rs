2df5ee1c96c72ee8684e18ba40f063d3a7edf084
Ainevsia <xuzhp0230@yeah.net>
2020-01-14 12:46:31 +0800

    follow the tutorial : 2d dp

--- 10. Regular Expression Matching/src/main.rs
+++ 10. Regular Expression Matching/src/main.rs
@@ -1,3 +1,26 @@
 fn main() {
     println!("Hello, world!");
 }
+
+struct Solution {}
+
+impl Solution {
+    pub fn is_match(s: String, p: String) -> bool {
+        
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Solution::is_match(String::from("aa"), String::from("a")), false);
+        assert_eq!(Solution::is_match(String::from("aa"), String::from("a*")), true);
+        assert_eq!(Solution::is_match(String::from("ab"), String::from(".*")), true);
+        assert_eq!(Solution::is_match(String::from("ab"), String::from("c*a*b")), true);
+        assert_eq!(Solution::is_match(String::from("mississippi"), String::from("mis*is*p*.")), false);
+        assert_eq!(Solution::is_match(String::from("mississippi"), String::from("mis*is*.p*.")), true);
+    }
+}
