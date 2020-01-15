17a1b59501274b2b3d0826d94e46fb31b6357768
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-15 08:22:11 +0800

    O(n) ! understand

--- 11. Container With Most Water/src/main.rs
+++ 11. Container With Most Water/src/main.rs
@@ -7,29 +7,18 @@ struct Solution {}
 
 impl Solution {
     pub fn max_area(height: Vec<i32>) -> i32 {
-        let n = height.len();
-        let mut area_max = 0;
-        for window_len in 1..n {
-            let mut area = 0;
-            for i in 0..n - window_len {
-                let min_height = if height[i] > height[i + window_len] {
-                    height[i + window_len]
-                } else {
-                    height[i]
-                };
-                area = if min_height * (window_len as i32) > area {
-                    min_height * (window_len as i32)
-                } else {
-                    area
-                };
-            }
-            area_max =  if area_max < area {
-                area
+        use std::cmp;
+        let (mut l, mut r) = (0, height.len() - 1);
+        let mut area = 0;
+        while l < r {
+            area = cmp::max(area, cmp::min(height[l], height[r]) * (r - l) as i32);
+            if height[l] < height[r] {
+                l += 1;
             } else {
-                area_max
-            };
+                r -= 1;
+            }
         }
-        area_max
+        area
     }
 }
 
