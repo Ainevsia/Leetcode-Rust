2686dd603e67488393ff89b5112fb21f8b76e3cd
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-27 14:21:23 +0800

    permuataion version . cannot even pass the time limit

---
+++ 30. Substring with Concatenation of All Words/src/main.rs
@@ -0,0 +1,127 @@
+fn main() {
+    Solution::find_substring(
+        "barfoothefoobarman".to_string(),
+        vec!["foo".to_string(), "bar".to_string(),]
+    );
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn find_substring(s: String, words: Vec<String>) -> Vec<i32> {
+        if s.len() == 0 || words.len() == 0 { return vec![] }
+        let substring_list: Vec<String> = words.into_iter().fold(
+            vec![vec![]],
+            |mut permutation, word| {
+                if permutation.len() == 0 {
+                    permutation.push(vec![word.clone()])
+                }
+                for _ in 0..permutation.len() {
+                    let base = permutation.remove(0);
+                    for i in 0..base.len() + 1 {
+                        let mut append = base.clone();
+                        append.insert(i, word.clone());
+                        permutation.push(append);
+                    }
+                }
+                permutation
+            }
+        ).into_iter().map( |words|
+            words.iter()
+                 .flat_map( |s| s.chars() )
+                 .collect::<String>()
+        ).collect();
+        let len = substring_list[0].len();
+        let mut res = vec![];
+        for i in 0..s.len() - len + 1 {
+            for j in 0..substring_list.len() {
+                if s[i..i + len] == substring_list[j] {
+                    res.push(i as i32);
+                    break
+                }
+            }
+        }
+        res 
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(
+            Solution::find_substring(
+                "barfoothefoobarman".to_string(),
+                vec!["foo".to_string(), "bar".to_string()]
+            ),
+            vec![0,9]
+        );
+
+        assert_eq!(
+            Solution::find_substring(
+                "wordgoodgoodgoodbestword".to_string(),
+                vec!["word".to_string(),
+                     "good".to_string(),
+                     "best".to_string(),
+                     "word".to_string(),]
+            ),
+            vec![]
+        );
+
+        assert_eq!(
+            Solution::find_substring(
+                "wordgoodgoodgoodbestword".to_string(),
+                vec!["word".to_string(),
+                     "good".to_string(),
+                     "best".to_string(),
+                     "good".to_string(),]
+            ),
+            vec![8]
+        );
+    }
+
+    #[test]
+    fn edge() {
+        assert_eq!(
+            Solution::find_substring(
+                "".to_string(),
+                vec![]
+            ),
+            vec![]
+        );
+
+        assert_eq!(
+            Solution::find_substring(
+                "123".to_string(),
+                vec![]
+            ),
+            vec![]
+        );
+
+        assert_eq!(
+            Solution::find_substring(
+                "".to_string(),
+                vec!["sdf".to_string()]
+            ),
+            vec![]
+        );
+    }
+
+    #[test]
+    fn largen() {
+        // assert_eq!(
+        //     Solution::find_substring(
+        //         "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel".to_string(),
+        //         ["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"]
+        //     ),
+        //     vec![]
+        // );
+        let x = Solution::find_substring(
+            "pjzkrkevzztxductzzxmxsvwjkxpvukmfjywwetvfnujhweiybwvvsrfequzkhossmootkmyxgjgfordrpapjuunmqnxxdrqrfgkrsjqbszgiqlcfnrpjlcwdrvbumtotzylshdvccdmsqoadfrpsvnwpizlwszrtyclhgilklydbmfhuywotjmktnwrfvizvnmfvvqfiokkdprznnnjycttprkxpuykhmpchiksyucbmtabiqkisgbhxngmhezrrqvayfsxauampdpxtafniiwfvdufhtwajrbkxtjzqjnfocdhekumttuqwovfjrgulhekcpjszyynadxhnttgmnxkduqmmyhzfnjhducesctufqbumxbamalqudeibljgbspeotkgvddcwgxidaiqcvgwykhbysjzlzfbupkqunuqtraxrlptivshhbihtsigtpipguhbhctcvubnhqipncyxfjebdnjyetnlnvmuxhzsdahkrscewabejifmxombiamxvauuitoltyymsarqcuuoezcbqpdaprxmsrickwpgwpsoplhugbikbkotzrtqkscekkgwjycfnvwfgdzogjzjvpcvixnsqsxacfwndzvrwrycwxrcismdhqapoojegggkocyrdtkzmiekhxoppctytvphjynrhtcvxcobxbcjjivtfjiwmduhzjokkbctweqtigwfhzorjlkpuuliaipbtfldinyetoybvugevwvhhhweejogrghllsouipabfafcxnhukcbtmxzshoyyufjhzadhrelweszbfgwpkzlwxkogyogutscvuhcllphshivnoteztpxsaoaacgxyaztuixhunrowzljqfqrahosheukhahhbiaxqzfmmwcjxountkevsvpbzjnilwpoermxrtlfroqoclexxisrdhvfsindffslyekrzwzqkpeocilatftymodgztjgybtyheqgcpwogdcjlnlesefgvimwbxcbzvaibspdjnrpqtyeilkcspknyylbwndvkffmzuriilxagyerjptbgeqgebiaqnvdubrtxibhvakcyotkfonmseszhczapxdlauexehhaireihxsplgdgmxfvaevrbadbwjbdrkfbbjjkgcztkcbwagtcnrtqryuqixtzhaakjlurnumzyovawrcjiwabuwretmdamfkxrgqgcdgbrdbnugzecbgyxxdqmisaqcyjkqrntxqmdrczxbebemcblftxplafnyoxqimkhcykwamvdsxjezkpgdpvopddptdfbprjustquhlazkjfluxrzopqdstulybnqvyknrchbphcarknnhhovweaqawdyxsqsqahkepluypwrzjegqtdoxfgzdkydeoxvrfhxusrujnmjzqrrlxglcmkiykldbiasnhrjbjekystzilrwkzhontwmehrfsrzfaqrbbxncphbzuuxeteshyrveamjsfiaharkcqxefghgceeixkdgkuboupxnwhnfigpkwnqdvzlydpidcljmflbccarbiegsmweklwngvygbqpescpeichmfidgsjmkvkofvkuehsmkkbocgejoiqcnafvuokelwuqsgkyoekaroptuvekfvmtxtqshcwsztkrzwrpabqrrhnlerxjojemcxel".to_string(),
+            vec!["dhvf","sind","ffsl","yekr","zwzq","kpeo","cila","tfty","modg","ztjg","ybty","heqg","cpwo","gdcj","lnle","sefg","vimw","bxcb"].iter().map(|x| x.to_string()).collect()
+        );
+        println!("x = {:#?}", x);
+    }
+}
