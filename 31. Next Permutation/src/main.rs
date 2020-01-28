cd8a412385a5470d86b90bd45348340e2acd787e
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-28 12:25:45 +0800

    ac

--- 31. Next Permutation/src/main.rs
+++ 31. Next Permutation/src/main.rs
@@ -6,28 +6,22 @@ struct Solution {}
 
 impl Solution {
     pub fn next_permutation(nums: &mut Vec<i32>) {
-        let mut i = nums.len() as i32 - 2;
-        while i >= 0 {
-            if nums[i as usize] < nums[i as usize + 1] {
-                break
+        if nums.len() <= 1 { return }
+        let mut i = nums.len() - 2;
+        while let false = nums[i] < nums[i + 1] {
+            if i != 0 {
+                i -= 1
             } else {
-                i -= 1;
+                nums.reverse();
+                return
             }
         }
-        if i < 0 {
-            nums.reverse()
-        } else {
-            let mut j = nums.len() - 1;
-            while i < j as i32 {
-                if nums[i as usize] < nums[j] {
-                    break
-                } else {
-                    j -= 1;
-                }
-            }
-            nums.swap(i as usize, j);
-            nums[i as usize + 1..j].reverse()
+        let mut j = nums.len() - 1;
+        while let false = nums[i] < nums[j] {
+            j -= 1;
         }
+        nums.swap(i, j);
+        nums[i+1..].reverse()
     }
 }
 
@@ -55,6 +49,24 @@ mod test {
     fn basic3() {
         let mut v1 = vec![1,1,5];
         let v2 = vec![1,5,1];
+        Solution::next_permutation(&mut v1);
+        assert_eq!(v1,v2);
+    }
+
+    #[test]
+    fn edge() {
+        let mut v1 = vec![1,1,1];
+        let v2 = vec![1,1,1];
+        Solution::next_permutation(&mut v1);
+        assert_eq!(v1,v2);
+
+        let mut v1 = vec![1];
+        let v2 = vec![1];
+        Solution::next_permutation(&mut v1);
+        assert_eq!(v1,v2);
+
+        let mut v1 = vec![];
+        let v2 = vec![];
         Solution::next_permutation(&mut v1);
         assert_eq!(v1,v2);
     }
