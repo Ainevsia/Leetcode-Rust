63b7756192c9737680068b05198e2097671bda69
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-01-28 18:13:16 +0800

    init

---
+++ 38. Count and Say/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn count_and_say(n: i32) -> String {
+        
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Solution::count_and_say(n: i32), );
+    }
+}
