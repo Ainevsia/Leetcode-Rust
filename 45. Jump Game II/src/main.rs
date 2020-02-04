301a74b9f73762e299c959b182967ccc24f81891
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-04 11:07:58 +0800

    linear ladder awesome

--- 45. Jump Game II/src/main.rs
+++ 45. Jump Game II/src/main.rs
@@ -6,20 +6,25 @@ struct Solution {}
 
 impl Solution {
     pub fn jump(nums: Vec<i32>) -> i32 {
-        if nums.len() <= 1 { return 0 }
-        let mut cnt = vec![i32::max_value(); nums.len()];
-        cnt[0] = 0;
-        for i in 0..nums.len() {
-            if i + nums[i] as usize >= nums.len() - 1 {
-                return cnt[i] + 1;
+        let nums: Vec<usize> = nums.iter().map(|x| *x as usize).collect();
+        let mut position = 0;
+        let mut cur_ladder = 0;
+        let mut next_ladder = nums[0];
+        let mut jump_cnt = 0;
+        while position < nums.len() - 1 {
+            if position + nums[position] > next_ladder {
+                next_ladder = position + nums[position];
             }
-            for idx in i+1..=i+nums[i] as usize {
-                if cnt[i] + 1 < cnt[idx] {
-                    cnt[idx] = cnt[i] + 1;
+            if position == cur_ladder {
+                jump_cnt += 1;
+                cur_ladder = next_ladder;
+                if cur_ladder >= nums.len() - 1 {
+                    return jump_cnt
                 }
             }
+            position += 1;
         }
-        unimplemented!()
+        0
     }
 }
 
