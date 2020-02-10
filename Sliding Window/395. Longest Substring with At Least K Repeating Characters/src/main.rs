9cc56a1e5cbd752104de0cfe8c562dca7be46d96
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-10 17:55:15 +0800

    recursive much faster??

--- Sliding Window/395. Longest Substring with At Least K Repeating Characters/src/main.rs
+++ Sliding Window/395. Longest Substring with At Least K Repeating Characters/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 struct Solution {}
 
 impl Solution {
-    pub fn longest_substring(s: String, k: i32) -> i32 {
+    pub fn longest_substring_template(s: String, k: i32) -> i32 {
         let s: Vec<char> = s.chars().collect();
         let mut res = 0;
         for n in 1..=26 {
@@ -34,6 +34,43 @@ impl Solution {
         }
         res as i32
     }
+
+    /// INSIGHT: any char that appears less than k times wont
+    /// be in the final array, which can be used to split the
+    /// string. Substrings has well property.
+    /// [Recursive] seem to be much faster >?
+    pub fn longest_substring(s: String, k: i32) -> i32 {
+        let s: Vec<char> = s.chars().collect();
+        Self::longest_substring_re(&s, k)
+    }
+
+    pub fn longest_substring_re(s: &[char], k: i32) -> i32 {
+        if s.len() < k as usize { return 0 }
+        use std::collections::{HashMap, HashSet};
+        let mut map: HashMap<char, i32> = HashMap::new();
+        for &c in s.iter() {
+            map.entry(c).and_modify(|x| *x += 1).or_insert(1);
+        }
+        let (less, _): (HashMap<char, i32>, _)= map.iter().partition(|(_, v)| **v < k);
+        let less: HashSet<char> = less.iter().map(|(k, _)| *k).collect();
+        if less.is_empty() { return s.len() as i32 }
+        let (mut start, mut end, mut res) = (0, 0, 0);
+        while end < s.len() {
+            if less.contains(&s[end]) {
+                res = i32::max(
+                    Self::longest_substring_re(&s[start..end], k),
+                    res
+                );
+                start = end + 1;
+            }
+            end += 1;
+        }
+        i32::max(
+            Self::longest_substring_re(&s[start..end], k),
+            res
+        )
+    }
+
 }
 
 #[cfg(test)]
@@ -51,7 +88,7 @@ mod test {
 
     }
     
-    #[test]
+    // #[test]
     fn basic_n() {
         assert_eq!(Solution::n_unique_char(&String::from("aaabb").chars().collect::<Vec<char>>(), 3, 1), 3);
         assert_eq!(Solution::n_unique_char(&String::from("ababbc").chars().collect::<Vec<char>>(), 2, 2), 5);
