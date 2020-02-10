845178c346a3781a998d039820702e174b78e785
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-10 21:21:16 +0800

    good ac

--- 995. Minimum Number of K Consecutive Bit Flips/src/main.rs
+++ 995. Minimum Number of K Consecutive Bit Flips/src/main.rs
@@ -1,28 +1,31 @@
 fn main() {
     Solution::min_k_bit_flips(vec![0,1,0], 1);
+    println!("-1%2 = {:#?}", -1%2);
 }
 
 struct Solution {}
 
 impl Solution {
-    pub fn min_k_bit_flips(mut a: Vec<i32>, k: i32) -> i32 {
-        if a.len() < k as usize { return -1 }
+    /// do you know what "code is comment itself" means ?
+    pub fn min_k_bit_flips(a: Vec<i32>, k: i32) -> i32 {
+        let k = k as usize;
         let mut res = 0;
-        for i in 0..=a.len() - k as usize {
-            if a[i] == 0 {
-                // flip
-                for j in 0..k as usize {
-                    a[i + j] = a[i + j] ^ 1;
-                }
+        let mut flipped = vec![0; a.len()];
+        let mut flip_in_pre_wnd = 0;
+        for i in 0..a.len() {
+            if i >= k {
+                // minus 1 if needed
+                flip_in_pre_wnd ^= flipped[i - k];
+            }
+            if flip_in_pre_wnd == a[i] {
+                // need to flip this bit
+                flip_in_pre_wnd ^= 1;   // pluss 1
+                flipped[i] = 1;
                 res += 1;
+                if i + k > a.len() { return -1 }
             }
         }
-        for i in a.len() - k as usize..a.len() {
-            if a[i] != 1 {
-                return -1;
-            }
-        }
-        res
+        if a.len() >0 { res } else { - 1 }
     }
 }
 
