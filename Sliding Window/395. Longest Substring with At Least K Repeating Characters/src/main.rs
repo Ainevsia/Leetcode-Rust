d554a6561619004a5b5e09ce0c5c7f5c840a71cc
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-10 17:17:59 +0800

    beautiful  ac

--- Sliding Window/395. Longest Substring with At Least K Repeating Characters/src/main.rs
+++ Sliding Window/395. Longest Substring with At Least K Repeating Characters/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    println!("Hello, world!");
+    Solution::longest_substring(String::from("aaabb"), 3);
 }
 
 struct Solution {}
@@ -7,21 +7,30 @@ struct Solution {}
 impl Solution {
     pub fn longest_substring(s: String, k: i32) -> i32 {
         let s: Vec<char> = s.chars().collect();
+        let mut res = 0;
+        for n in 1..=26 {
+            res = i32::max(Self::n_unique_char(&s, k, n), res);
+        }
+        res
+    }
+
+    pub fn n_unique_char(s: &[char], k: i32, n: usize) -> i32 {
         use std::collections::HashMap;
         let mut map: HashMap<char, i32> = HashMap::new();
         let (mut start, mut end, mut res) = (0, 0, 0);
-        for &c in s.iter() {
-            map.entry(c).and_modify(|x| *x += 1).or_insert(1);
-        }
-        let (less, _): (HashMap<char, i32>, HashMap<char, i32>) = map.iter().partition(|(_, v)| **v < k);
-        if less.is_empty() { return s.len() as i32 }
         while end < s.len() {
-            if less.get(&s[end]).is_some() {
-                // split here
-                res = usize::max(end - start, res);
-                start = end + 1;
+            map.entry(s[end]).and_modify(|x| *x += 1).or_insert(1);
+            end += 1;
+            while map.len() > n {
+                map.entry(s[start]).and_modify(|x| *x -= 1);
+                if map.get(&s[start]).unwrap() == &0 {
+                    map.remove(&s[start]);
+                }
+                start += 1;
             }
-            end += 1; 
+            if map.iter().all(|(_, v)| *v >= k) {
+                res = usize::max(end - start, res);
+            }
         }
         res as i32
     }
@@ -38,7 +47,18 @@ mod test {
         assert_eq!(Solution::longest_substring(String::from("aaabbb"), 3), 6);
         assert_eq!(Solution::longest_substring(String::from("weitong"), 2), 0);
         assert_eq!(Solution::longest_substring(String::from("ababacb"), 3), 0);
+        assert_eq!(Solution::longest_substring(String::from("bbaaacbd"), 3), 3);
 
     }
     
+    #[test]
+    fn basic_n() {
+        assert_eq!(Solution::n_unique_char(&String::from("aaabb").chars().collect::<Vec<char>>(), 3, 1), 3);
+        assert_eq!(Solution::n_unique_char(&String::from("ababbc").chars().collect::<Vec<char>>(), 2, 2), 5);
+        assert_eq!(Solution::n_unique_char(&String::from("aaabbb").chars().collect::<Vec<char>>(), 3, 2), 6);
+        assert_eq!(Solution::n_unique_char(&String::from("weitong").chars().collect::<Vec<char>>(), 2, 1), 0);
+        assert_eq!(Solution::n_unique_char(&String::from("ababacb").chars().collect::<Vec<char>>(), 3, 3), 0);
+        assert_eq!(Solution::n_unique_char(&String::from("bbaaacbd").chars().collect::<Vec<char>>(), 3, 1), 3);
+
+    }
 }
