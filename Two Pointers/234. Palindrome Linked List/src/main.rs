e64b792d1de77968c76dae1f95fe2f9ae902b8a1
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-14 10:33:53 +0800

    two pointers ac

--- Two Pointers/234. Palindrome Linked List/src/main.rs
+++ Two Pointers/234. Palindrome Linked List/src/main.rs
@@ -1,5 +1,12 @@
 fn main() {
-    println!("Hello, world!");
+    let n = ListNode::new(2);
+    let n = ListNode { val: 1, next: Some(Box::new(n)) };
+    assert_eq!(Solution::is_palindrome(Some(Box::new(n))), false);
+    let n = ListNode::new(1);
+    let n = ListNode { val: 2, next: Some(Box::new(n)) };
+    let n = ListNode { val: 2, next: Some(Box::new(n)) };
+    let n = ListNode { val: 1, next: Some(Box::new(n)) };
+    assert_eq!(Solution::is_palindrome(Some(Box::new(n))), true);
 }
 
 struct Solution {}
@@ -17,10 +24,34 @@ impl ListNode {
         ListNode { next: None, val }
     }
 }
+
 impl Solution {
     pub fn is_palindrome(head: Option<Box<ListNode>>) -> bool {
-        
-        unimplemented!()
+        if head.is_none() { return true }
+        let mut slow = head.as_ref();
+        let mut fast = head.as_ref();
+        loop {
+            // fast should not be none
+            if let Some(node) = fast.unwrap().next.as_ref() {
+                if node.next.is_some() {
+                    fast = node.next.as_ref();
+                } else { break }
+            } else { break }
+            slow = slow.unwrap().next.as_ref();
+        }
+        slow = slow.unwrap().next.as_ref();
+        let mut vec = vec![];
+        while let Some(node) = slow {
+            vec.push(node.val);
+            slow = node.next.as_ref();
+        }
+        println!("{:?}", vec);
+        let mut ptr = head.as_ref();
+        for &v in vec.iter().rev() {
+            if ptr.is_none() || ptr.unwrap().val != v { return false }
+            ptr = ptr.unwrap().next.as_ref();
+        }
+        true
     }
 }
 
@@ -30,6 +61,16 @@ mod test {
 
     #[test]
     fn basic() {
-        assert_eq!(Solution::, );
+        let n = ListNode::new(2);
+        assert_eq!(Solution::is_palindrome(Some(Box::new(n))), true);
+        let n = ListNode::new(2);
+        let n = ListNode { val: 1, next: Some(Box::new(n)) };
+        assert_eq!(Solution::is_palindrome(Some(Box::new(n))), false);
+        let n = ListNode::new(1);
+        let n = ListNode { val: 2, next: Some(Box::new(n)) };
+        let n = ListNode { val: 2, next: Some(Box::new(n)) };
+        let n = ListNode { val: 1, next: Some(Box::new(n)) };
+        assert_eq!(Solution::is_palindrome(Some(Box::new(n))), true);
+        assert_eq!(Solution::is_palindrome(None), true);
     }
 }
