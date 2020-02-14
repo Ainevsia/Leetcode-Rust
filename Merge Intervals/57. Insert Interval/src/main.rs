09beb57496d0b52dc5159b3beee55c73db0dabd7
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-14 17:36:10 +0800

    inplace not good

---
+++ Merge Intervals/57. Insert Interval/src/main.rs
@@ -0,0 +1,34 @@
+fn main() {
+    assert_eq!(Solution::insert(vec![vec![1,3],vec![6,9]], vec![2,5]), vec![vec![1,5],vec![6,9]]);
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn insert(mut intervals: Vec<Vec<i32>>, mut new_interval: Vec<i32>) -> Vec<Vec<i32>> {
+        let mut i = 0;
+        while i < intervals.len() {
+            if intervals[i][1] < new_interval[0] { i += 1; continue }
+            if intervals[i][0] > new_interval[1] { intervals.insert(i, new_interval); return intervals }
+            if intervals[i][0] < new_interval[0] { new_interval[0] = intervals[i][0]; }
+            if intervals[i][1] > new_interval[1] { new_interval[1] = intervals[i][1]; }
+            intervals.remove(i);
+        }
+        intervals.push(new_interval);
+        intervals
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Solution::insert(vec![vec![1,3],vec![6,9]], vec![2,5]), vec![vec![1,5],vec![6,9]]);
+        assert_eq!(Solution::insert(vec![vec![1,2],vec![3,5],vec![6,7],vec![8,10],vec![12,16]], vec![4,8]), vec![vec![1,2],vec![3,10],vec![12,16]]);
+        assert_eq!(Solution::insert(vec![vec![1,2]], vec![4,8]), vec![vec![1,2],vec![4,8]]);
+        assert_eq!(Solution::insert(vec![], vec![4,8]), vec![vec![4,8]]);
+
+    }
+}
