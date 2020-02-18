95727ff1ecbabeecdf8d2dc4d9618ba0d008527b
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-18 22:51:22 +0800

    wowoowo! I cannot believe it. it is my dfa! it is my regular expression!

--- 65. Valid Number/src/main.rs
+++ 65. Valid Number/src/main.rs
@@ -38,13 +38,70 @@ impl Solution {
         
         true
     }
-    pub fn is_number(s: String) -> bool {
+
+    pub fn is_number_re(s: String) -> bool {
         // this regex works
-        let re = Regex::new(r"^ *(\+|-)?(\d+\.\d*|\d*\.\d+|\d+)(e(\+|-)?\d+)? *$").unwrap();
+        let re = Regex::new(r"^(\+|-)?(\d+(\.\d*)?|\.\d+)(e(\+|-)?\d+)?$").unwrap();
         println!("s = {:#?}", s);
-        re.is_match(&s)
+        re.is_match(s.trim())
+    }
+
+    pub fn is_number(s: String) -> bool {
+        let mut state = S1;
+        for c in s.trim().chars() {
+            match state {
+                S1 => {
+                    match c {
+                        '+' | '-' => state = S2,
+                        '.' => state = S3,
+                        _ => if "0123456789".contains(c) { state = S4 } else { return false },
+                    }
+                }
+                S2 => {
+                    match c {
+                        '.' => state = S3,
+                        _ => if "0123456789".contains(c) { state = S4 } else { return false },
+                    }
+                }
+                S3 => {
+                    if "0123456789".contains(c) { state = S5 } else { return false }
+                }
+                S4 => {
+                    match c {
+                        '.' => state = S5,
+                        'e' => state = S6,
+                        _ => if "0123456789".contains(c) { state = S4 } else { return false },
+                    }
+                }
+                S5 => {
+                    match c {
+                        'e' => state = S6,
+                        _ => if "0123456789".contains(c) { state = S5 } else { return false },
+                    }
+                }
+                S6 => {
+                    match c {
+                        '+' | '-' => state = S7,
+                        _ => if "0123456789".contains(c) { state = S8 } else { return false },
+                    }
+                }
+                S7 => {
+                    if "0123456789".contains(c) { state = S8 } else { return false }
+                }
+                S8 => {
+                    if "0123456789".contains(c) { state = S8 } else { return false }
+                }
+            }
+        }
+        // println!("s = {:#?}, state = {:#?}", s, state);
+        if state == S4 || state == S5 || state == S8 { true } else { false }
     }
 }
+
+#[derive(PartialEq, Debug)]
+enum State {S1, S2, S3, S4, S5, S6, S7, S8}
+
+use State::{S1, S2, S3, S4, S5, S6, S7, S8};
 
 #[cfg(test)]
 mod test {
@@ -94,19 +151,19 @@ mod test {
         assert_eq!(Solution::is_number(String::from("0.00000")), true);
         assert_eq!(Solution::is_number(String::from("0.")), true);  //Cannot be more disagree!!!
         assert_eq!(Solution::is_number(String::from("00.5")), true);  //Strongly cannot agree
-        // assert_eq!(Solution::is_number(String::from("123e1")), true);
-        // assert_eq!(Solution::is_number(String::from("1.23e10")), true);
-        // assert_eq!(Solution::is_number(String::from("0.5e-10")), true);
-        // assert_eq!(Solution::is_number(String::from("1.0e4.5")), false);
-        // assert_eq!(Solution::is_number(String::from("0.5e04")), true);
-        // assert_eq!(Solution::is_number(String::from("12 3")), false);
-        // assert_eq!(Solution::is_number(String::from("1a3")), false);
-        // assert_eq!(Solution::is_number(String::from("")), false);
-        // assert_eq!(Solution::is_number(String::from("   ")), false);
-        // assert_eq!(Solution::is_number(String::from(".1")), true); //Ok)), if you say so
-        // assert_eq!(Solution::is_number(String::from(".")), false);
-        // assert_eq!(Solution::is_number(String::from("2e0")), true);  //Really?!
-        // assert_eq!(Solution::is_number(String::from("+.8")), true);  
-        // assert_eq!(Solution::is_number(String::from(" 005047e+6")), true);  //Damn = =|||
+        assert_eq!(Solution::is_number(String::from("123e1")), true);
+        assert_eq!(Solution::is_number(String::from("1.23e10")), true);
+        assert_eq!(Solution::is_number(String::from("0.5e-10")), true);
+        assert_eq!(Solution::is_number(String::from("1.0e4.5")), false);
+        assert_eq!(Solution::is_number(String::from("0.5e04")), true);
+        assert_eq!(Solution::is_number(String::from("12 3")), false);
+        assert_eq!(Solution::is_number(String::from("1a3")), false);
+        assert_eq!(Solution::is_number(String::from("")), false);
+        assert_eq!(Solution::is_number(String::from("   ")), false);
+        assert_eq!(Solution::is_number(String::from(".1")), true); //Ok)), if you say so
+        assert_eq!(Solution::is_number(String::from(".")), false);
+        assert_eq!(Solution::is_number(String::from("2e0")), true);  //Really?!
+        assert_eq!(Solution::is_number(String::from("+.8")), true);  
+        assert_eq!(Solution::is_number(String::from(" 005047e+6")), true);  //Damn = =|||
     }
 }
