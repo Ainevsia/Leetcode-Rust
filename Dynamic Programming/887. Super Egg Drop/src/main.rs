246b56b2ab0b6d17eb05284631ce59e99dca10f8
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-21 16:47:50 +0800

    this problem knock me down

--- Dynamic Programming/887. Super Egg Drop/src/main.rs
+++ Dynamic Programming/887. Super Egg Drop/src/main.rs
@@ -5,30 +5,36 @@ fn main() {
 struct Solution {}
 
 impl Solution {
+    /// https://leetcode.com/problems/super-egg-drop/discuss/158974/C%2B%2BJavaPython-2D-and-1D-DP-O(KlogN)
+    /// Original DP definition: I stand on nth floor and give me k eggs, 
+    /// the minimum times I try is dp[n][k]. This definition means the result of 
+    /// this problem is dp[N][K].
+    /// 
+    /// This solution is somehow a reverse thinking:
+    /// New DP definition: If you give me k egg, let me drop m times, 
+    /// I can try out maximum dp[m][k] floors. Based on this definition, 
+    /// the result is some m, which cases dp[m][K] equals N.
+    /// 
+    /// The transfer equation is based on the following facts:
+    /// No matter which floor you try, the egg will only break or not break, 
+    /// if break, go downstairs, 
+    /// if not break, go upstairs.
+    /// 
+    /// No matter you go up or go down, the num of all the floors is always 
+    /// upstairs + downstairs + the floor you try, which is 
+    /// dp[m][k] = dp[m - 1][k - 1] + dp[m - 1][k] + 1.
+    /// 
     pub fn super_egg_drop(k: i32, n: i32) -> i32 {
-        let mut dp = vec![vec![0; n as usize + 1]; k as usize + 1];
-        for i in 1..=n as usize {
-            dp[1][i] = i as i32;
-        }
-        for i in 2..=k as usize {
-            dp[i][1] = 1;
-            for j in 2..=n as usize {
-                // use binary search to reduce the complexity to O(KN logN)
-                let (mut start, mut end) = (0, j-1);
-                let mut m = (start + end) / 2;
-                while start + 1 != end {  // breaks when start + 1 == end
-                    if dp[i][m] < dp[i-1][j-1-m] { start = m }
-                    else if dp[i][m] == dp[i-1][j-1-m] { break }
-                    else if dp[i][m] > dp[i-1][j-1-m] { end = m }
-                    m = (start + end) / 2;
-                }
-                // end = start + 1  or  dp[i-1][m] == dp[i][m]
-                dp[i][j] = std::cmp::min(std::cmp::max(dp[i][m], dp[i-1][j-1-m]),
-                                         std::cmp::max(dp[i][j-m-1], dp[i-1][j-1-m]) ) + 1;
+        let (K, N) = (k as usize, n as usize);
+        let mut dp = vec![vec![0; K + 1]; N + 1];
+        let mut m = 0;
+        while dp[m][K] < N {
+            m += 1;
+            for k in 1..=K {
+                dp[m][k] = dp[m-1][k-1] + dp[m-1][k] + 1
             }
         }
-        // println!("dp = {:#?}", dp);
-        dp[k as usize][n as usize]
+        m as i32
     }
 }
 
