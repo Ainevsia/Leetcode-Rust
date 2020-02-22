87e16fd05fca7533e333c574b1e5515f706c90cb
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-22 21:54:59 +0800

    space optimize

--- 72. Edit Distance/src/main.rs
+++ 72. Edit Distance/src/main.rs
@@ -5,23 +5,27 @@ fn main() {
 struct Solution {}
 
 impl Solution {
+    /// 2d dp -> reduce to O(n) space
+    /// Time: O(mn)
+    /// not clear and beautiful as before
     pub fn min_distance(word1: String, word2: String) -> i32 {
         use std::cmp::min;
         let word1: Vec<char> = word1.chars().collect();
         let word2: Vec<char> = word2.chars().collect();
-        let mut dp = vec![vec![0; word2.len()+1]; word1.len()+1];
-        // dp need init
-        for i in 1..=word1.len() { dp[i][0] = i }
+        let mut dp = vec![vec![0; word2.len()+1]; 2];
         for j in 1..=word2.len() { dp[0][j] = j }
+        dp[1][0] = 1;
         for i in 1..=word1.len() {
             for j in 1..=word2.len() {
-                if word1[i-1] == word2[j-1] { dp[i][j] = dp[i-1][j-1] }
+                if word1[i-1] == word2[j-1] { dp[1][j] = dp[0][j-1] }
                 else {
-                    dp[i][j] = min(min(dp[i-1][j], dp[i][j-1]), dp[i-1][j-1]) + 1
+                    dp[1][j] = min(min(dp[0][j], dp[0][j-1]), dp[1][j-1]) + 1
                 }
             }
+            dp[0] = dp[1].clone();
+            dp[1][0] = 1 + i;
         }
-        dp[word1.len()][word2.len()] as i32
+        dp[0][word2.len()] as i32
     }
 }
 
@@ -33,6 +37,7 @@ mod test {
     fn basic() {
         assert_eq!(Solution::min_distance(String::from("horse"), String::from("ros")), 3);
         assert_eq!(Solution::min_distance(String::from("intention"), String::from("execution")), 5);
+        assert_eq!(Solution::min_distance(String::from("b"), String::from("")), 1);
 
     }
 }
