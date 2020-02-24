4fda86e6222e3fff18448bfe4024fe1138044f2a
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-24 11:09:19 +0800

    imporve shell

--- script.sh
+++ script.sh
@@ -8,15 +8,25 @@ function recur() {
 		folder=${folder//'&'/" "}
         if [[ `echo "$folder" | grep "[0-9]\."` ]]   
         then  
-    		echo "$folder" 
-			if [[ `ls "$1""/""$folder" | grep "cpp"` ]]
+			# this is a problem folder, not problem class folder 
+    		echo -n "| ${folder//'/'/''} |" 
+			if [[ `ls "$1""$folder" | grep "toml"` ]]
 			then
-				echo "cpp"
+				# this is a rust project
+				echo -n "[Rust](${1//'/home/ainevsia/gh/Leetcode-Rust'/'.'}${folder//' '/"%20"}src/main.rs)|"
+			else
+				echo -n "|"
+			fi
+			if [[ `ls "$1""$folder" | grep "cpp"` ]]
+			then
+				# this project contains cpp Solution
+				echo -n "[C++](${1//'/home/ainevsia/gh/Leetcode-Rust'/'.'}${folder//' '/"%20"})|"
 			fi
         else 
-			recur "$1""/""$folder"
+			# this is problem class folder , recur
+			recur "$1""$folder"
         fi  
     done  
 }
 
-recur $CWD 
+recur $CWD'/'
