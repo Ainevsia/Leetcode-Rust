2775f49e6df5db115e560a46bc3bf58e910d0cc9
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-29 18:24:10 +0800

    cyclic sort

--- 448. Find All Numbers Disappeared in an Array/Solution.cpp
+++ 448. Find All Numbers Disappeared in an Array/Solution.cpp
@@ -10,13 +10,11 @@ public:
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> res ;
         for (int i=0; i< nums.size(); ) {
-            cout << i;
             if (nums[i] != i + 1 and nums[nums[i] - 1] != nums[i]) {
                 //swap
-                int t = nums[i];
+                int t = nums[i], p = nums[i] - 1;
                 nums[i] = nums[nums[i] - 1];
-                nums[nums[i] - 1] = t;              // buggy code ! here nums[i] changed
-                                                    // in the previous line !
+                nums[p] = t;
             } else i ++ ;
         }
         for (int i=0; i<nums.size(); i++) {
@@ -29,9 +27,6 @@ public:
 int main() {
     Solution s;
     vector<int> v {4,3,2,7,8,2,3,1};
-    cout << "hello" << endl;
     auto x = s.findDisappearedNumbers(v);
-    for (auto i: x) {
-        cout << i << endl;
-    }
+    for (auto i: x) cout << i << endl;
 }
