de2ca7ef25387d547b4f66dd790de3116b3053b6
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-02-29 22:14:58 +0800

    greddy

---
+++ 122. Best Time to Buy and Sell Stock II/Solution.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    // greedy ok
+    int maxProfit(vector<int>& prices) {
+        if (prices.size() <= 1) return 0;
+        int maxprofit = 0, minprice = prices[0];
+        int sum = 0;
+        for (int i=1; i<prices.size(); i++) {
+            if (prices[i] < prices[i-1]) {
+                sum += maxprofit;
+                minprice = prices[i];
+                maxprofit = 0;
+                continue;
+            }
+            minprice = prices[i] < minprice ? prices[i] : minprice;
+            maxprofit = prices[i] - minprice > maxprofit ? prices[i] - minprice : maxprofit;
+        }
+        return sum + maxprofit;
+    }
+
+};
+
+int main() {
+    Solution s;
+
+}
