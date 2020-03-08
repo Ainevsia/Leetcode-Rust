162810e52b83aa1a8a38a1928c80176224c00f69
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-08 13:59:49 +0800

    initial commit

--- Weekly Contest 179/p3/Solution.cpp
+++ Weekly Contest 179/p3/Solution.cpp
@@ -9,7 +9,31 @@
 #include <cmath>
 using namespace std;
 
-
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        vector<vector<int>> subo (n, vector<int>());
+        for (int i=0; i<n; i ++) {
+            if (manager[i] != -1) {
+                subo[manager[i]].push_back(i);
+            }
+        }
+        vector<int> time(n, 0); // time to get informed
+        deque<int> send;
+        send.push_back(headID);
+        while (not send.empty()) {
+            int sender = send.front();
+            send.pop_front();
+            for (auto i : subo[sender]) {
+                time[i] = time[sender] + informTime[sender];
+                send.push_back(i);
+            }
+        }
+        int res = 0;
+        for (auto i : time) if (i > res) res = i;
+        return res;
+    }
+};
 
 int main() {
     Solution s;
