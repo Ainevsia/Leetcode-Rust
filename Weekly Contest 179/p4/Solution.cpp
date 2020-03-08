d6c276174f0ea9bb275d3bf104ccad72f11e35c3
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-08 14:55:06 +0800

    dfs

--- Weekly Contest 179/p4/Solution.cpp
+++ Weekly Contest 179/p4/Solution.cpp
@@ -10,31 +10,49 @@
 using namespace std;
 
 class Solution {
+private:
+    vector<vector<int>> ut; // undirected tree
+    vector<bool> visited;
+    int target;
+    double p = 0.0;
 public:
-    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
-        vector<vector<int>> childs (n, vector<int>());
-        deque<int> knownfather ;
-        knownfather.push_back(1);
-        while (not knownfather.empty()) {
-            int father = knownfather.front();
-            knownfather.pop_front();
-            for (auto edge : edges) {
-                int from = edge[0], to = edge[1];
-                if (from == father) {
-                    childs[father].push_back(to);
-                    knownfather.push_back(to);
-                } 
-                else if (to == father) {
-                    childs[father].push_back(from);
-                    knownfather.push_back(from);
-                }
-            }
+    /* get the number of childs of the node id */
+    int getchildn(int id) {
+        for (vector<int>::iterator it = ut[id].begin(); it < ut[id].end(); ) {
+            if (visited[*it]) ut[id].erase(it);
+            else it ++ ;
         }
-        double p = 1;
-        int round = 0;
-        while (++round <= t)
-        
-        
+        visited[id] = true;
+        return ut[id].size();
+    }
+
+    // p -> possibility to reach this node `id`
+    void dfs(int id, double p, int round_left) {
+        if (round_left == 0) {  // cannot travel any farther
+            if (id == target) this->p = p;
+            return;
+        } 
+        int childn = getchildn(id);
+        if (id == target and childn == 0) { // reach target and nowhere else to go
+            this->p = p;
+            return;
+        } else if (id == target or childn == 0) return;
+        p /= childn;
+        for (int cid : ut[id])
+            dfs(cid, p, round_left-1);
+    }
+
+    double frogPosition(int n, vector<vector<int>>& edges, int t, int target) {
+        visited.resize(n+1, false);
+        ut.resize(n+1);
+        this->target = target;
+        for (vector<int> edge : edges) {
+            int from = edge[0], to = edge[1];
+            ut[from].push_back(to);
+            ut[to].push_back(from);
+        }
+        dfs(1, 1, t);
+        return p;
     }
 };
 
