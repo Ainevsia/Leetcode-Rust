40a33e5909dd9a24815cb0e666727d4d0731f654
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-09 09:11:33 +0800

    never use magic number !

--- 99. Recover Binary Search Tree/Solution.cpp
+++ 99. Recover Binary Search Tree/Solution.cpp
@@ -17,32 +17,23 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
+// never use magic number !
 class Solution {
 private:
     TreeNode * sw1;
     TreeNode * sw2;
+    TreeNode * prev;
 public:
-    bool isValid(TreeNode* root, int mi, int ma, bool llimit, bool rlimit) {
-        if (root == NULL) return true;
-        if (llimit and root->val <= mi or rlimit and root->val >= ma) {
-            sw2 = root;
-            return false;
-        }
-        bool lis_valid = isValid(root->left, mi, min(ma, root->val), llimit, true);
-        bool ris_valid = isValid(root->right, max(mi, root->val), ma, true, rlimit);
-        if (not lis_valid and not ris_valid) {
-            sw1 = root->left;
-            sw2 = root->right;
-            return false;
-        } else if (not (lis_valid and ris_valid)) {
-            sw1 = root;
-            return false;
-        } else return true;
-    }
-    
-    bool isValidBST(TreeNode* root) {
-        int ma = ((unsigned int) ~0) >> 1, mi = 1 << 31;
-        return isValid(root, mi, ma, false, false);
+    Solution():sw1(NULL), sw2(NULL), prev(new TreeNode(1 << 31)) {}
+
+    void traverse(TreeNode * root) {
+        if (root == NULL) return ;
+        traverse(root->left);
+        // in-order traverse
+        if (sw1 == NULL and prev->val >= root->val) sw1 = prev;
+        if (sw1 != NULL and prev->val >= root->val) sw2 = root;
+        prev = root;
+        traverse(root->right);
     }
 
     void swap() {
@@ -52,7 +43,7 @@ public:
     }
 
     void recoverTree(TreeNode* root) {
-        isValidBST(root);
+        traverse(root);
         swap();
     }
 };
