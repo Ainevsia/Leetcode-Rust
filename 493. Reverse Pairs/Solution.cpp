392811b856fbd94ec59e1dd9020315e8dd2f988f
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-09 21:58:34 +0800

    bug bug bug!

--- 493. Reverse Pairs/Solution.cpp
+++ 493. Reverse Pairs/Solution.cpp
@@ -13,39 +13,41 @@ class Solution {
 public:
     int rev_pair;
     vector<int> n;
-public:
     Solution():rev_pair(0) {}
 
     void mergeSort(int l, int r) {
+        // cout << l << ' ' << r << endl;
         if (l + 1 >= r) return; // zero or one element
         if (l + 2 == r) {
-            int large = max(n[l], n[l + 1]);
-            int small = min(n[l], n[l + 1]);
-            if (small != n[l]) {
-                rev_pair ++;
-                n[l] = small;
-                n[l + 1] = large;
+            if (n[l] >= n[l + 1]) {
+                if (n[l] > n[l + 1] * 2L) rev_pair ++ ;
+                n[l] ^= n[l + 1];
+                n[l + 1] ^= n[l];
+                n[l] ^= n[l + 1];
             }
-        } else {
-            int m = (l + r) / 2;
-            mergeSort(l, m);
-            mergeSort(m, r);
-            // merge the two arrays
-            vector<int> buf;
-            buf.reserve(r - l);
-            int i = 0, j = 0;
-            while (i + l < m and j + m < r) {
-                if (n[i + l] > n[j + m]) {
-                    buf.push_back(n[j++ + m]);
-                    rev_pair += m - l - i;
-                } else buf.push_back(n[i++ + l]);
-            }
-            while (i + l < m) buf.push_back(n[i++ + l]);
-            while (j + m < r) buf.push_back(n[j++ + m]);
-            for (int i = 0; i < buf.size(); i++)
-                n[i + l] = buf[i];
+            return;
         }
-
+        int m = (l + r) >> 1;
+        mergeSort(l, m);
+        mergeSort(m, r);
+        // merge the two arrays
+        vector<int> buf;
+        buf.reserve(r - l);
+        int i = 0, j = 0;
+        while (i + l < m and j + m < r) {
+            if (n[i + l] > n[j + m] * 2L) {
+                rev_pair += m - l - i;
+                j ++ ;
+            } else i++ ;
+        }
+        for (i = j = 0; i + l < m and j + m < r;) {
+            if (n[i + l] > n[j + m]) buf.push_back(n[j++ + m]);
+            else buf.push_back(n[i++ + l]);
+        }
+        while (i + l < m) buf.push_back(n[i++ + l]);
+        while (j + m < r) buf.push_back(n[j++ + m]);
+        for (int i = 0; i < buf.size(); i++)
+            n[i + l] = buf[i];
     }
 
     int reversePairs(vector<int>& nums) {
@@ -58,9 +60,11 @@ public:
 
 int main() {
     Solution a;
-    vector <int > v {1,3,2,3,1};
+    vector <int > v {-5,-5};
     int x = a.reversePairs(v);
     cout << x;
+    for (auto i:a.n) cout << i << ' ';
+    cout << (-1 << 1);
     return 0;
 }
 
