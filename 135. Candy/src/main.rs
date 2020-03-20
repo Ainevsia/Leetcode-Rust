0fa7c6304ba0d7b3dd7c1ce6f254fecb8cf3bea9
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-20 13:54:55 +0800

    two pass solution with O(n) time and O(n) space

--- 135. Candy/src/main.rs
+++ 135. Candy/src/main.rs
@@ -5,29 +5,21 @@ fn main() {
 struct Solution {}
 
 impl Solution {
-    /// Bad solution O(n^2) time O(n) space
+    /// two pass solution with O(n) time and O(n) space
     pub fn candy(ratings: Vec<i32>) -> i32 {
-        if ratings.len() == 0 { return 0 }
-        if ratings.len() == 1 { return 1 }
-        let mut v = vec![0; ratings.len()];
+        if ratings.len() <= 1 { return ratings.len() as i32 }
+        let mut v = vec![1; ratings.len()];
         for i in 1..ratings.len() {
             if ratings[i] > ratings[i-1] {
                 v[i] = v[i-1] + 1;
-            } else if ratings[i] <= ratings[i-1] {
-                v[i] = i32::min(v[i-1]-1, 0);
-            }
-            if v[i] < 0 {
-                // uplevel the previous
-                let mut j = i - 1;
-                while j != 0 && ratings[j] > ratings[j+1] && v[j] == v[j+1] + 1 { j -= 1 }
-                if ratings[j] <= ratings[j + 1] || v[j] > v[j+1] + 1 { j += 1 }
-                while j <= i {
-                    v[j] += 1;
-                    j += 1;
-                }
             }
         }
-        v.iter().sum::<i32>() + ratings.len() as i32
+        for i in (0..ratings.len()-1).rev() {
+            if ratings[i] > ratings[i+1] && v[i] < v[i+1] + 1 {
+                v[i] = v[i+1] + 1;
+            }
+        }
+        v.iter().sum()
     }
 }
 
