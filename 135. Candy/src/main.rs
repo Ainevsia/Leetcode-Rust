7d6a7f58afbd3be3a5c02c7cb1cd6b70c85e4f95
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-20 13:41:40 +0800

    Bad solution O(n^2) time O(n) space

--- 135. Candy/src/main.rs
+++ 135. Candy/src/main.rs
@@ -5,25 +5,29 @@ fn main() {
 struct Solution {}
 
 impl Solution {
-    /// a wrong solution
+    /// Bad solution O(n^2) time O(n) space
     pub fn candy(ratings: Vec<i32>) -> i32 {
         if ratings.len() == 0 { return 0 }
         if ratings.len() == 1 { return 1 }
-        let mut sum = 0;
-        let mut prev_candy = 0;
-        let mut min_candy = 0;
+        let mut v = vec![0; ratings.len()];
         for i in 1..ratings.len() {
-            let this_candy;
-            if ratings[i] <= ratings[i-1] {
-                this_candy = i32::min(prev_candy - 1, 0);
-            } else {
-                this_candy = prev_candy + 1;
+            if ratings[i] > ratings[i-1] {
+                v[i] = v[i-1] + 1;
+            } else if ratings[i] <= ratings[i-1] {
+                v[i] = i32::min(v[i-1]-1, 0);
             }
-            if this_candy < min_candy { min_candy = this_candy }
-            sum += this_candy;
-            prev_candy = this_candy;
+            if v[i] < 0 {
+                // uplevel the previous
+                let mut j = i - 1;
+                while j != 0 && ratings[j] > ratings[j+1] && v[j] == v[j+1] + 1 { j -= 1 }
+                if ratings[j] <= ratings[j + 1] || v[j] > v[j+1] + 1 { j += 1 }
+                while j <= i {
+                    v[j] += 1;
+                    j += 1;
+                }
+            }
         }
-        sum + ratings.len() as i32 * (1 + i32::abs(min_candy))
+        v.iter().sum::<i32>() + ratings.len() as i32
     }
 }
 
@@ -35,10 +39,11 @@ mod test {
     fn basic() {
         assert_eq!(Solution::candy(vec![1,0,2]), 5);
         assert_eq!(Solution::candy(vec![1,2,2]), 4);
+        assert_eq!(Solution::candy(vec![1,3,2,2,1]), 7);
     }
 
     #[test]
     fn fail() {
-        assert_eq!(Solution::candy(vec![1,3,2,2,1]), 7);
+        assert_eq!(Solution::candy(vec![1,2,3,1,0]), 9);
     }
 }
