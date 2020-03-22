079cf683f13f4114e6dc272fad9e7108a2560509
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-22 11:27:45 +0800

    ugly

--- Weekly Contest 181/p2/Solution.cpp
+++ Weekly Contest 181/p2/Solution.cpp
@@ -9,8 +9,66 @@
 #include <cmath>
 #include <queue>
 using namespace std;
+enum From {up, down, _left, _right} ;
 
-
+class Solution {
+public:
+    int m;
+    int n;
+    
+    bool go(vector<vector<int>>& grid, int i, int j, From from) {
+        if (i < 0 or i >= m or j < 0 or j >= n) return false;
+        if (i == 0 and j == 0) // loop
+            return false;
+        bool isfinal =( i + 1 == m and j + 1 == n );
+        switch (grid[i][j]) {
+            case 1:
+                if (from == _left) return go (grid, i, j+1, _left) or isfinal;
+                if (from == _right) return go(grid, i, j-1, _right) or isfinal;
+                break;
+            case 2:
+                if (from == up) return go (grid, i+1, j, up) or isfinal;
+                if (from == down) return go(grid, i-1, j, down) or isfinal;
+                break;
+            case 3:
+                if (from == _left) return go (grid, i+1, j, up) or isfinal;
+                if (from == down) return go(grid, i, j-1, _right) or isfinal;
+                break;
+            case 4:
+                if (from == _right) return go (grid, i+1, j, up) or isfinal;
+                if (from == down) return go(grid, i, j+1, _left) or isfinal;
+                break;
+            case 5:
+                if (from == _left) return go (grid, i-1, j, down) or isfinal;
+                if (from == up) return go(grid, i, j-1, _right) or isfinal;
+                break;
+            case 6:
+                if (from == up) return go (grid, i, j+1, _left) or isfinal;
+                if (from == _right) return go(grid, i-1, j, down) or isfinal;
+                break;
+        }
+        return false;
+    }
+    
+    bool hasValidPath(vector<vector<int>>& grid) {
+        m = grid.size();
+        n = grid[0].size();
+        if (m == 1 and n == 1) return true;
+        switch (grid[0][0]) {
+            case 2:
+            case 3:
+                return go(grid, 1, 0, up);
+            case 5:
+                return false;
+            case 1:
+            case 6:
+                return go(grid, 0, 1, _left);
+            case 4:
+                return go(grid, 0, 1, _left) or go(grid, 1, 0, up);
+        }
+        return false;
+    }
+};
 
 int main() {
     Solution a;
--- Weekly Contest 181/p4/Solution.cpp
+++ Weekly Contest 181/p4/Solution.cpp
@@ -10,7 +10,29 @@
 #include <queue>
 using namespace std;
 
-
+class Solution {
+public:
+    // both end inclusive
+    bool _equal(string & s1, int i1, int j1, string & s2, int i2, int j2) {
+        if (j1 - i1 != j2 - i2) return false;
+        int len = j1 - i1 + 1;
+        for (int i = 0; i < len ; i++) {
+            if (s1[i1+i] != s2[i2+i]) return false;
+        }
+        return true;
+    }
+    
+    string longestPrefix(string s) {
+        int len = s.size();
+        int maxl = 0;
+        for (int i =0; i+1<len; i++) {
+            if (_equal(s,0,i, s,len-1-i,len-1)) maxl = i+1;
+        }
+        string res;
+        res.assign(s.begin(),s.begin()+maxl);
+        return res;
+    }
+};
 
 int main() {
     Solution a;
