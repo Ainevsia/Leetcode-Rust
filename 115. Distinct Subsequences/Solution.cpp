c890650f0e1fb36682c87ec2d34ce709c508ee5a
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-03-24 14:04:16 +0800

    string dp

---
+++ 115. Distinct Subsequences/Solution.cpp
@@ -0,0 +1,37 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <sstream>
+#include <algorithm>
+#include <tuple>
+#include <deque>
+#include <unordered_map>
+#include <cmath>
+#include <queue>
+using namespace std;
+
+class Solution {
+public:
+    // string dp
+    int numDistinct(string s, string t) {
+        vector <vector<long long>> dp(t.size()+1, vector<long long>(s.size()+1, 0L));
+        for (auto &i : dp[0]) i = 1;
+        for (int i=1;i<=t.size();i++)
+            for (int j=1;j<=s.size();j++)
+                if (t[i-1] == s[j-1])
+                    dp[i][j] = dp[i-1][j-1] + dp [i][j-1];
+                else 
+                    dp[i][j] = dp[i][j-1];
+        return dp[t.size()][s.size()];
+    }
+};
+
+int main() {
+    Solution a;
+    string s = "xslledayhxhadmctrliaxqpokyezcfhzaskeykchkmhpyjipxtsuljkwkovmvelvwxzwieeuqnjozrfwmzsylcwvsthnxujvrkszqwtglewkycikdaiocglwzukwovsghkhyidevhbgffoqkpabthmqihcfxxzdejletqjoxmwftlxfcxgxgvpperwbqvhxgsbbkmphyomtbjzdjhcrcsggleiczpbfjcgtpycpmrjnckslrwduqlccqmgrdhxolfjafmsrfdghnatexyanldrdpxvvgujsztuffoymrfteholgonuaqndinadtumnuhkboyzaqguwqijwxxszngextfcozpetyownmyneehdwqmtpjloztswmzzdzqhuoxrblppqvyvsqhnhryvqsqogpnlqfulurexdtovqpqkfxxnqykgscxaskmksivoazlducanrqxynxlgvwonalpsyddqmaemcrrwvrjmjjnygyebwtqxehrclwsxzylbqexnxjcgspeynlbmetlkacnnbhmaizbadynajpibepbuacggxrqavfnwpcwxbzxfymhjcslghmajrirqzjqxpgtgisfjreqrqabssobbadmtmdknmakdigjqyqcruujlwmfoagrckdwyiglviyyrekjealvvigiesnvuumxgsveadrxlpwetioxibtdjblowblqvzpbrmhupyrdophjxvhgzclidzybajuxllacyhyphssvhcffxonysahvzhzbttyeeyiefhunbokiqrpqfcoxdxvefugapeevdoakxwzykmhbdytjbhigffkmbqmqxsoaiomgmmgwapzdosorcxxhejvgajyzdmzlcntqbapbpofdjtulstuzdrffafedufqwsknumcxbschdybosxkrabyfdejgyozwillcxpcaiehlelczioskqtptzaczobvyojdlyflilvwqgyrqmjaeepydrcchfyftjighntqzoo";
+    string t = "rwmimatmhydhbujebqehjprrwfkoebcxxqfktayaaeheys";
+    int x = a.numDistinct(s,t);
+    cout << "result" << x;
+    return 0;
+}
+
