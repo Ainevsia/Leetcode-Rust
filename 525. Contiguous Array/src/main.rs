8672563cb6ea28253b8fbe9b7d0bb27b6a127356
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-04-16 22:51:32 +0800

    O(n) using map (array)

--- 525. Contiguous Array/src/main.rs
+++ 525. Contiguous Array/src/main.rs
@@ -9,22 +9,31 @@ impl Solution {
         if nums.is_empty() { return 0 }
         let mut sum = 0;
         let mut height = Vec::with_capacity(nums.len() + 1);
-        height.push(0);
-        for i in nums {
-            sum += if i == 0 { -1 } else { 1 };
-            height.push(sum);
+        let mut map: Vec<(usize, usize)> = Vec::with_capacity(nums.len() * 2 + 1);
+        for _ in 0..map.capacity() {
+            map.push((usize::max_value(), usize::max_value()));
         }
-        let mut max_idx = 1;
-        let mut res = 0;
-        for i in 0..height.len() {
-            if i + 1 > max_idx {
-                max_idx = i + 1;
+        height.push(0);
+        let base = nums.len();
+        map[base + 0].0 = 0;
+        
+        // O (n)
+        for i in 0..nums.len() {
+            sum += if nums[i] == 0 { -1 } else { 1 };
+            height.push(sum);
+            let idx: usize = (base as i32 + sum) as usize;
+            if map[idx].0 == usize::max_value() {
+                map[idx].0 = i + 1;
+            } else {
+                map[idx].1 = i + 1;
             }
-            for j in max_idx..height.len() {
-                if height[j] == height[i] && j - i > res {
-                    res = j - i;
-                    max_idx = j;
-                }
+        }
+        let mut res = 0;
+
+        // O (2n+1)
+        for i in 0..map.len() {
+            if map[i].1 != usize::max_value() && map[i].1 - map[i].0 > res {
+                res = map[i].1 - map[i].0;
             }
         }
         res as i32
