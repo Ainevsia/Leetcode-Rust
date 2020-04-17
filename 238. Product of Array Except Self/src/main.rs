3455a77b72091a0ca2c88e1f72030258ce0ff5b4
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-04-17 14:26:16 +0800

    init : ( too much projects)

---
+++ 238. Product of Array Except Self/src/main.rs
@@ -0,0 +1,21 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Solution {}
+
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use crate::*;
+
+    #[test]
+    fn basic() {
+        assert_eq!(Solution::, );
+    }
+}
