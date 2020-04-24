edec6e184c5db26a1eb1e62098605a30cd3e8210
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-04-24 14:07:27 +0800

    use the output vector to achieve O(n) space

--- 238. Product of Array Except Self/src/main.rs
+++ 238. Product of Array Except Self/src/main.rs
@@ -1,12 +1,24 @@
 fn main() {
-    println!("Hello, world!");
+    assert_eq!(Solution::product_except_self(vec![1,2,3,4]), [24,12,8,6]);
 }
 
 struct Solution {}
 
 impl Solution {
+    /// use the output vector to achieve O(n) space
     pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
-        
+        let mut res = vec![1];
+        let mut x = 1;
+        for i in 0..nums.len() - 1 {
+            x *= nums[i];
+            res.push(x);
+        }
+        let mut x = 1;
+        for i in (0..nums.len()).rev() {
+            res[i] = res[i] * x;
+            x *= nums[i];
+        }
+        res
     }
 }
 
@@ -16,6 +28,6 @@ mod test {
 
     #[test]
     fn basic() {
-        assert_eq!(Solution::, );
+        assert_eq!(Solution::product_except_self(vec![1,2,3,4]), [24,12,8,6]);
     }
 }
