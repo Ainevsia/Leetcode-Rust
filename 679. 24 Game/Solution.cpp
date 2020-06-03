9c5c2334511f533023e4d80659c7b9d450ed27a5
Ainevsia <zhipengxu@sjtu.edu.cn>
2020-06-03 22:04:21 +0800

    chang to clang

--- 679. 24 Game/Solution.cpp
+++ 679. 24 Game/Solution.cpp
@@ -1,6 +1,7 @@
 #include <string>
 #include <vector>
 #include <iostream>
+#include <stdio.h>
 #include <sstream>
 #include <algorithm>
 #include <tuple>
@@ -11,7 +12,7 @@
 #include <queue>
 using namespace std;
 
-enum Ops {Add, Minus, Mul, Div};
+enum Ops {Add, Minus, Mul, Div, NotValid};
 
 Ops operator++ (Ops & rs, int) {
     Ops old_enum = rs;
@@ -25,15 +26,20 @@ public:
         for (int i = 0; i < 24; i ++ ) {
             next_permutation(nums);
             int x1 = nums[0], x2 = nums[1], x3 = nums[2], x4 = nums[3];
-            // cout << x1 << x2 << x3 << x4 << endl;
+            // printf("%d %d %d %d\n", x1, x2, x3, x4);
             for (Ops i = Add; i <= Div; i ++) {
                 for (Ops j = Add; j <= Div; j ++) {
                     for (Ops k = Add; k <= Div; k ++) {
-                        // cout << i << j << k << endl;
+                        // if (x1 == 6 and x2 == 1 and x3 == 3 and x4 == 4 and 
+                        //     i == Div and j == Minus and k == Div) {
+                        //         cout << to_str(i) << to_str(j) << to_str(k) << endl;
+                        //         cout << abs(oper(i, oper(j, oper(k, x3, x4), x2), x1)) << endl;
+                        //     }
+                            
                         if (1e-5 > abs(oper(k, oper(j, oper(i, x1, x2), x3), x4) - 24) ||
                             1e-5 > abs(oper(j, oper(i, x1, x2), oper(k, x3, x4)) - 24) ||
-                            1e-5 > abs(oper(i, oper(k, oper(j, x2, x3), x4), x1) - 24) ||
-                            1e-5 > abs(oper(i, oper(j, oper(k, x3, x4), x2), x1) - 24) )
+                            1e-5 > abs(oper(i, x1, oper(k, oper(j, x2, x3), x4)) - 24) ||
+                            1e-5 > abs(oper(i, x1, oper(j, x2, oper(k, x3, x4))) - 24) )
                             return true;
                     }
                 }
@@ -52,15 +58,19 @@ public:
                 return x * y;
             case Div:
                 return x / y;
+            default:
+                return x;
         }
     }
 
     void next_permutation(vector<int> & nums) {
         int i = nums.size() - 1;
         while (i >= 0) {
-            if (i != 0 and nums[i - 1] < nums[i]) {
+            if (i != 0 and nums[i - 1] < nums[i] ) {
+                int j = 3;
+                while (nums[j] <= nums[i - 1]) j -- ;
+                swap(nums[i - 1], nums[j]);
                 reverse(nums.begin() + i, nums.end());
-                swap(nums[i - 1], nums[3]);
                 return;
             } else if (i == 0) {
                 reverse(nums.begin(), nums.end());
@@ -70,17 +80,36 @@ public:
         }
     }
 
+    string to_str(Ops op) {
+        switch (op) {
+            case Add:
+                return "Add ";
+            case Minus:
+                return "Minus ";
+            case Mul:
+                return "Mul ";
+            case Div:
+                return "Div ";
+            default:
+                return "Not ";
+        }
+    }
 };
 
 int main() {
     Solution a;
     vector<int> v ;
+    vector<vector<int>> b;
+    b.push_back(v);
     v.clear();
-    v.push_back(4);
+    // v.push_back(4);
+    // v.push_back(1);
+    // v.push_back(8);
+    // v.push_back(7);
     v.push_back(1);
-    v.push_back(8);
-    v.push_back(7);
-    
+    v.push_back(3);
+    v.push_back(4);
+    v.push_back(6);
     auto x = a.judgePoint24(v);
     cout << x;
     return 0;
